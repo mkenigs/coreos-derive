b107f81acf91931dd47467bc95d129cc0955a8f9
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-01-19 13:33:06 -0600

    Build Ignition in container and add rpm overrides

--- prototype/build-fcos.sh
+++ prototype/build-fcos.sh
@@ -4,52 +4,61 @@ source ./variables.sh
 
 FCOS="$PWD/fcos"
 
+cosa() {
+   set +eu
+   env | grep COREOS_ASSEMBLER
+   local -r COREOS_ASSEMBLER_CONTAINER_LATEST="quay.io/coreos-assembler/coreos-assembler:latest"
+   if [[ -z ${COREOS_ASSEMBLER_CONTAINER} ]] && $(podman image exists ${COREOS_ASSEMBLER_CONTAINER_LATEST}); then
+       local -r cosa_build_date_str="$(podman inspect -f "{{.Created}}" ${COREOS_ASSEMBLER_CONTAINER_LATEST} | awk '{print $1}')"
+       local -r cosa_build_date="$(date -d ${cosa_build_date_str} +%s)"
+       if [[ $(date +%s) -ge $((cosa_build_date + 60*60*24*7)) ]] ; then
+         echo -e "\e[0;33m----" >&2
+         echo "The COSA container image is more that a week old and likely outdated." >&2
+         echo "You should pull the latest version with:" >&2
+         echo "podman pull ${COREOS_ASSEMBLER_CONTAINER_LATEST}" >&2
+         echo -e "----\e[0m" >&2
+         sleep 10
+       fi
+   fi
+   podman run --rm -ti --security-opt label=disable --privileged                                    \
+              --uidmap=1000:0:1 --uidmap=0:1:1000 --uidmap 1001:1001:64536                          \
+              -v ${PWD}:/srv/ --device /dev/kvm --device /dev/fuse                                  \
+              --tmpfs /tmp -v /var/tmp:/var/tmp --name cosa                                         \
+              ${COREOS_ASSEMBLER_CONFIG_GIT:+-v $COREOS_ASSEMBLER_CONFIG_GIT:/srv/src/config/:ro}   \
+              ${COREOS_ASSEMBLER_GIT:+-v $COREOS_ASSEMBLER_GIT/src/:/usr/lib/coreos-assembler/:ro}  \
+              ${COREOS_ASSEMBLER_CONTAINER_RUNTIME_ARGS}                                            \
+              ${COREOS_ASSEMBLER_CONTAINER:-$COREOS_ASSEMBLER_CONTAINER_LATEST} "$@"
+   rc=$?; set +x; return $rc
+   set -eu
+}
+
 ensure_fcos() {
+
   if skopeo inspect "$FCOS_IMAGE" &> /dev/null; then
     return
   fi
 
   mkdir "$FCOS"
-  
-  git clone -b pr/apply --single-branch https://github.com/jlebon/ignition
-  pushd ignition
-    make install DESTDIR="$FCOS/overrides/rootfs"
-  popd
-  
-  cosa() {
-     env | grep COREOS_ASSEMBLER
-     local -r COREOS_ASSEMBLER_CONTAINER_LATEST="quay.io/coreos-assembler/coreos-assembler:latest"
-     if [[ -z ${COREOS_ASSEMBLER_CONTAINER} ]] && $(podman image exists ${COREOS_ASSEMBLER_CONTAINER_LATEST}); then
-         local -r cosa_build_date_str="$(podman inspect -f "{{.Created}}" ${COREOS_ASSEMBLER_CONTAINER_LATEST} | awk '{print $1}')"
-         local -r cosa_build_date="$(date -d ${cosa_build_date_str} +%s)"
-         if [[ $(date +%s) -ge $((cosa_build_date + 60*60*24*7)) ]] ; then
-           echo -e "\e[0;33m----" >&2
-           echo "The COSA container image is more that a week old and likely outdated." >&2
-           echo "You should pull the latest version with:" >&2
-           echo "podman pull ${COREOS_ASSEMBLER_CONTAINER_LATEST}" >&2
-           echo -e "----\e[0m" >&2
-           sleep 10
-         fi
-     fi
-     set -x
-     podman run --rm -ti --security-opt label=disable --privileged                                    \
-                --uidmap=1000:0:1 --uidmap=0:1:1000 --uidmap 1001:1001:64536                          \
-                -v ${PWD}:/srv/ --device /dev/kvm --device /dev/fuse                                  \
-                --tmpfs /tmp -v /var/tmp:/var/tmp --name cosa                                         \
-                ${COREOS_ASSEMBLER_CONFIG_GIT:+-v $COREOS_ASSEMBLER_CONFIG_GIT:/srv/src/config/:ro}   \
-                ${COREOS_ASSEMBLER_GIT:+-v $COREOS_ASSEMBLER_GIT/src/:/usr/lib/coreos-assembler/:ro}  \
-                ${COREOS_ASSEMBLER_CONTAINER_RUNTIME_ARGS}                                            \
-                ${COREOS_ASSEMBLER_CONTAINER:-$COREOS_ASSEMBLER_CONTAINER_LATEST} "$@"
-     rc=$?; set +x; return $rc
-  }
-  
   pushd "$FCOS"
     cosa init https://github.com/coreos/fedora-coreos-config
     cosa fetch
+  popd
+  
+  git clone -b pr/apply --single-branch https://github.com/jlebon/ignition
+
+  if ! skopeo inspect containers-storage:localhost/ignition-builder &> /dev/null; then
+    buildah from --name ignition-builder registry.fedoraproject.org/fedora-toolbox
+    buildah run ignition-builder dnf install -y make libblkid-devel go
+    buildah commit ignition-builder ignition-builder
+  fi
+  podman run -v "$PWD":/coreos-derive localhost/ignition-builder make -C /coreos-derive/ignition install DESTDIR="/coreos-derive/fcos/overrides/rootfs"
+
+  curl --create-dirs  --output-dir "$FCOS/overrides/rpm" -O https://jenkins-coreos-ci.apps.ocp.ci.centos.org/job/github-ci/job/coreos/job/rpm-ostree/job/PR-3340/3/artifact/rpm-ostree-2022.1.41.gc1bd10d2-1.fc35.x86_64.rpm 
+  curl --output-dir "$FCOS/overrides/rpm" -O https://jenkins-coreos-ci.apps.ocp.ci.centos.org/job/github-ci/job/coreos/job/rpm-ostree/job/PR-3340/3/artifact/rpm-ostree-libs-2022.1.41.gc1bd10d2-1.fc35.x86_64.rpm
+  
+  pushd "$FCOS"
     cosa build
   popd
 
   skopeo copy oci-archive:$(ls "$FCOS"/builds/latest/x86_64/*.ociarchive) "$FCOS_IMAGE"
-  rm -rf "$FCOS"
-  rm -rf ignition
 }
