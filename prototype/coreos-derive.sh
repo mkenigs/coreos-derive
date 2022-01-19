e0dc5d9e430bdcaca445dadca31e63dfc608270d
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-01-19 13:40:14 -0600

    Add layer with rpm

--- prototype/coreos-derive.sh
+++ prototype/coreos-derive.sh
@@ -6,6 +6,7 @@ source ./variables.sh
 IGNITION=/usr/lib/dracut/modules.d/30ignition/ignition
 WORKING_CONTAINER=coreos-derive
 TEST_IGN_PATH="/etc/test.ign"
+TEST_TREEFILE_PATH="/etc/rpm-ostree-layer.yaml"
 
 source ./build-fcos.sh
 ensure_fcos
@@ -15,4 +16,8 @@ buildah from --name "$WORKING_CONTAINER" "$FCOS_IMAGE"
 buildah copy "$WORKING_CONTAINER" test.ign "$TEST_IGN_PATH"
 # container=1: tell ignition-liveapply we in a container so it doesn't fail
 buildah run --env container=1 "$WORKING_CONTAINER" -- sh -c "exec -a ignition-liveapply $IGNITION $TEST_IGN_PATH"
+buildah commit "$WORKING_CONTAINER" with-ignition
+buildah copy "$WORKING_CONTAINER" treefile.yaml "$TEST_TREEFILE_PATH"
+buildah run "$WORKING_CONTAINER" rpm-ostree compose container "$TEST_TREEFILE_PATH"
 buildah commit "$WORKING_CONTAINER" layer1
+buildah commit "$WORKING_CONTAINER" with-rpms
