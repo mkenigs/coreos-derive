c779ae766f12e38a2ee627e48a1b2a161b103a3a
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-01-25 16:28:29 -0600

    Use mkenigs/fcos:v0.1 throughout pipeline

--- prototype/coreos-derive.sh
+++ prototype/coreos-derive.sh
@@ -7,7 +7,7 @@ IGNITION=/usr/lib/dracut/modules.d/30ignition/ignition
 WORKING_CONTAINER=coreos-derive
 IGN_PATH="/etc/$TRANSPILED_IGN"
 
-buildah from --name "$WORKING_CONTAINER" "$FCOS_IMAGE"
+buildah from --name "$WORKING_CONTAINER" "$VERSIONED_FCOS_IMAGE"
 buildah copy "$WORKING_CONTAINER" "$TRANSPILED_IGN" "$IGN_PATH"
 # container=1: tell ignition-liveapply we in a container so it doesn't fail
 buildah run --env container=1 "$WORKING_CONTAINER" -- sh -c "exec -a ignition-liveapply $IGNITION $IGN_PATH"
--- prototype/derive_pipeline.sh
+++ prototype/derive_pipeline.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 source ./variables.sh
 
 # not sure yet whether butane will be in the fcos image and coreos-derive, but for now just lumping it into the fcos image
-podman run -v "$PWD":/coreos-derive --security-opt label=disable "$FCOS_IMAGE" butane --pretty --strict /coreos-derive/input.bu -o /coreos-derive/"$TRANSPILED_IGN"
+podman run -v "$PWD":/coreos-derive --security-opt label=disable "$VERSIONED_FCOS_IMAGE" butane --pretty --strict /coreos-derive/input.bu -o /coreos-derive/"$TRANSPILED_IGN"
 ./coreos-derive.sh
